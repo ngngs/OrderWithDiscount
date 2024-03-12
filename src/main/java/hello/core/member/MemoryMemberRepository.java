824d642c67e518cfde5380042286f79cdc8f6964
Gyuseong Moon <ngng-s@hanmail.net>
2024-03-13 00:19:11 +0900

    [ADD]회원 도메인 생성

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    // 동시성 이슈를 고려하여, Concurrent 해시맵을 사용해야함
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
