b52df303306a764775ea37941e3b28e1e0901efa
Gyuseong Moon <ngng-s@hanmail.net>
2024-04-28 19:35:41 +0900

    [feat] @PostConstruct, @PreDestory 방식을 활용한 생성과 소멸

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
@@ -27,12 +30,14 @@ public class NetworkClient {
         System.out.println("close " + url);
     }
 
+    @PostConstruct
     public void init(){
         System.out.println("NetwordClient.after");
         connect();
         call("초기화 연결 ");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.destroy");
         disconnect();
